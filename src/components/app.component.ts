21712c630453f1ebefbb05993034aa8e202f4363
Admin Apple <adminapple@ackerlaptop.local>
2019-05-09 16:02:26 -0400

    x

--- src/components/app.component.ts
+++ src/components/app.component.ts
@@ -1,5 +1,5 @@
-import {  Component } from "@angular/core"
-import { string } from "./templates/app.pug"
+import {  Component } from '@angular/core'
+import { string } from './templates/app.pug'
 
 @Component({
   selector:"app",
